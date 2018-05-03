642364225801918550b162cd720905e94a1e8b36
Roberto Monesi <mone.monesi.mm@gmail.com>
2018-05-03 20:28:15 +0100

    Refactoring HW1 and upload starting files for HW2

--- src/MyTests.java
+++ src/MyTests.java
@@ -1,44 +1,36 @@
+import static org.junit.Assert.assertEquals;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
-/**
- * This class contains some test cases to help you write your code
- * 
- * You are encouraged to write larger test cases!
- * Note that when you submit, our tests also check that you've implementing the three
- * functions correctly (they make an appropriate number of comparisons)
- */
 public class MyTests {
-
-	/**
-	 * Helper function for converting an array of ints to an array of our ComapreInts
+	
+	/*TODO: Add your own test cases here!
+	 * We've provided a sample test case for each problem below
+	 * You can use these as building blocks to write your own test cases
 	 */
-	private static CompareInt[] convert(int[] arr) {
-		CompareInt[] newArr = new CompareInt[arr.length];
-		for (int i = 0; i < newArr.length; i++) {
-			newArr[i] = new CompareInt(arr[i]);
-		}
-		return newArr;
+	
+	@Test
+	public void HuffmanSampleTest() {
+		String input = "abc";
+		Huffman h = new Huffman(input);
+		String encoding = h.encode();
+		assertEquals(input, h.decode(encoding));
+		assertEquals("huffman abc compression", Huffman.compressionRatio(input), 0.20833, 0.01);
 	}
 	
 	@Test
-	public void KthSmallestTestSampleHeapTest() {
-		CompareInt[] arr = convert(new int[]{4, 1, 3});
-		assertEquals(3, KthSmallest.heapImpl(2, arr));
+	public void IntervalSampleTest() {
+		GreedyDynamicAlgorithms.Interval red = new GreedyDynamicAlgorithms.Interval(1, 3);
+		GreedyDynamicAlgorithms.Interval blue = new GreedyDynamicAlgorithms.Interval(0, 4);
+		ArrayList<GreedyDynamicAlgorithms.Interval> reds = new ArrayList<>();
+		ArrayList<GreedyDynamicAlgorithms.Interval> blues = new ArrayList<>();
+		reds.add(red);
+		blues.add(blue);
+		int expectedOptimal = 1;
+		int actualOptimal = GreedyDynamicAlgorithms.optimalIntervals(reds, blues);
+		assertEquals("interval 1 red 1 blue", expectedOptimal, actualOptimal);
 	}
-	
-	@Test
-	public void KthSmallestTestSampleMergeTest() {
-		CompareInt[] arr = convert(new int[]{4, 1, 3});
-		assertEquals(3, KthSmallest.mergeSortImpl(2, arr));
-	}
-	
-	@Test
-	public void KthSmallestTestSampleQuickSelectTest() {
-		CompareInt[] arr = convert(new int[]{4, 1, 3});
-		assertEquals(3, KthSmallest.quickSelectImpl(2, arr));
-	}
-	
+
 }
